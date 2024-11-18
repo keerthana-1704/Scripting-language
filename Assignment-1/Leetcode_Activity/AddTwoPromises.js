0e830a5762061a3324a459a922b99df7c10376c6
keerthana-1704 <keerthanakumar109@gmail.com>
2024-11-18 11:56:18 +0530

    Delete Assignment-1/Leetcode_Activity/AddTwoPromises.js

--- Assignment-1/Leetcode_Activity/AddTwoPromises.js
+++
@@ -1,7 +0,0 @@
-/*Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.*/
-  
-var addTwoPromises = async function(promise1, promise2) {
-    const result1 = await promise1;
-    const result2 = await promise2;
-    return result1 + result2; 
-};
